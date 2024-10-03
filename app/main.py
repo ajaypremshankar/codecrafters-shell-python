06a187ead1a62606574639659df329bf80bdd751
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2024-10-03 22:29:16 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 
+supported_commands = ["exit", "echo", "type"]
 
 def main():
     while True:
@@ -11,13 +12,22 @@ def main():
 
         command_and_params = command.split(" ")
 
-        if command_and_params[0] == 'exit':
-            sys.exit(int(command_and_params[1]))
-        elif command_and_params[0] == 'echo':
-            print(" ".join(command_and_params[1:]))
+        if command_and_params[0] in supported_commands:
+            handle_command(command_and_params[0], command_and_params[1:])
         else:
             print(f"{command}: not found")
 
+
+def handle_command(command: str, args: [str]):
+    if command == 'exit':
+        sys.exit(int(args[0]))
+    elif command == 'echo':
+        print(" ".join(args))
+    elif command == 'type':
+        if args[0] in supported_commands:
+            print(f"{args[0]} is a shell builtin")
+        else:
+            print(f"{args[0]}: not found")
 
 
 if __name__ == "__main__":
