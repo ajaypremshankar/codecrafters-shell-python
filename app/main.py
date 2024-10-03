95c53ba58211cde409e5f9ca84d3973ec782512f
Ajay Prem Shankar <ajaypremshankar@gmail.com>
2024-10-04 00:08:33 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, subprocess
 
 supported_commands = ["exit", "echo", "type"]
 path_str = os.environ.get("PATH")
@@ -22,11 +22,18 @@ def handle_command(paths: [str], command: str, args: [str]):
     if command == 'exit':
         sys.exit(int(args[0]))
     elif command == 'echo':
-        print(" ".join(args))
+        to_echo = " ".join(args)
+        sys.stdout.write(f"{to_echo}\n")
     elif command == 'type':
         handle_type_command(paths, args[0])
     else:
-        print(f"{command}: not found")
+        custom_command = get_custom_command_if_exists(command=command)
+
+        if custom_command is None:
+            sys.stdout.write(f"{command}: not found\n")
+        else:
+            result = subprocess.run([custom_command, *args], capture_output=True, text=True)
+            sys.stdout.write(result.stdout)
 
 
 def handle_type_command(paths: [str], target_command: str):
@@ -37,19 +44,18 @@ def handle_type_command(paths: [str], target_command: str):
                 command_path_key = f"{path}/{target_command}"
     
     if target_command in supported_commands:
-        print(f"{target_command} is a shell builtin")
+        sys.stdout.write(f"{target_command} is a shell builtin\n")
     elif command_path_key is not None:
-        print(f"{target_command} is {command_path_key}")
+        sys.stdout.write(f"{target_command} is {command_path_key}\n")
     else:
-        print(f"{target_command}: not found")
+        sys.stdout.write(f"{target_command}: not found\n")
 
-def has_path(command: str):
-    return True if command.startswith("PATH=") else False
-
-def get_paths(path: str):
-    paths_str = path.split("=")[1]
-    return paths_str.split(":")
-
-
+def get_custom_command_if_exists(command: str):
+    paths = path_str.split(":")
+    if len(paths) > 0:
+        for path in paths:
+            if os.path.isfile(f"{path}/{command}"):
+                return f"{path}/{command}"
+    return None
 if __name__ == "__main__":
     main()
